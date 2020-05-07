d599364d69c73c4956b38b63787c6efe7432d207
bereketh <berekethailemariam95@gmail.com>
2020-05-06 21:27:05 -0700

    first commit

---
+++ src/com/bereket/Main.java
@@ -0,0 +1,52 @@
+package com.bereket;
+/*
+Create an abstract Student class with the following fields
+- student ID
+- fname
+- lname
+- email
+
+Give it the following methods
+- constructor
+- getters
+
+Create a BiologyStudent class and a EconStudent class that extend
+the abstract class. These should both just have a constructor that
+invokes the super class.
+
+Create a Classrooms class and give it a generic type that extends Student.
+Instances of this class should specify the subject. For example, biologyClass
+and econClass. Each classroom should only be able to add students for that
+subject.
+
+This class should have the following fields:
+
+- name
+- classroomSize - this represents the number of enrolled students. Initialize
+this to 0. This will be incremented each time a student is added.
+- ArrayList of students - give it the same generic type as the Classroom
+class declaration
+
+The class should have the following methods:
+
+- Constructor that only takes a name
+- Method to add a student. It takes a student object using the same generic
+type as the Classroom class decleration
+- Method that returns the number of students enrolled
+
+ In your main method, show me that you've tested your code.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+    // BiologyStudent TimJones = new BiologyStudent(1234,"Tim","Jones","tjones@gmail.com");
+    //    Classrooms<BiologyStudent> students = new Classrooms<>("AP BIO");
+    //    students.addStudent(TimJones);
+    //    EconStudent BobSmith = new EconStudent(2873,"Bob","Smith","bsmith@gmail.com");
+    //    Classrooms<EconStudent> students2 = new Classrooms<>("AP Econ");
+    //    students2.addStudent(TimJones);
+    //    System.out.println(students.numStudents());
+    //    System.out.println(students2.numStudents());
+    }
+}
